7f808fcebfc546047a4b96ec8c1f8f2a8149487c
Emraanjadidi <emraan.jadidi@outlook.com>
2020-01-17 20:08:56 +0330

    Initial

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string message)
+        {
+            var connectionId = this.Context.ConnectionId;
+            await Clients.All.SendAsync("ReciveMessage", message, connectionId);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var connectionId = this.Context.ConnectionId;
+            Clients.Client(connectionId).SendAsync("SetConnection", connectionId);
+
+            return base.OnConnectedAsync();
+        }
+    }
+}
---
+++ SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,34 @@
+﻿
+var myConnectionId = "";
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReciveMessage", function (message, ConnectionId) {
+    $('[name="Message"]').val('');
+    if (myConnectionId === ConnectionId) {
+        $('#MyMessage').append('<li class="list-group-item">' + message + '</li>')
+    }
+    else {
+        $('#ReciveMessage').append('<li class="list-group-item">' + message + '</li>')
+    }
+})
+
+connection.start().then(function () {
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("SetConnection", function (connectionId) {
+    myConnectionId = connectionId;
+    console.log(myConnectionId);
+})
+
+$('#send').on('click', function (e) {
+
+    var message = $('[name="Message"]').val();
+
+    connection.invoke("SendMessageToAll", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    e.preventDefault();
+})
